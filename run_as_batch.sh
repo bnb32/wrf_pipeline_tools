6f430c191cf1994b5ecc777236b73b1b78f8b77b
Brandon N Benton <b.benton19@gmail.com>
2021-12-19 14:19:43 -0800

    Delete run_as_batch.sh

--- run_as_batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 4 ]; then echo "usage $0: function batch_id time args"; exit 1; fi
-
-FUNC=$1; NAME=$2; TIME=$3; ARGS=$4
-
-cd ~/RUNWORKFLOW/logs
-qsub -N ${NAME} -A UCOR0023 -l "$TIME" -q regular -j oe -m abe -M bnb.chey.mon@gmail.com -l select=1:ncpus=1:mpiprocs=1 -S /bin/bash -v $ARGS $FUNC
-
